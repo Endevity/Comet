92ff2312d5f8d69459bad6bdfb3fa73b63d54aba
Adam Varady <146221892+NotRaijin@users.noreply.github.com>
2024-06-25 23:44:41 +0200

    MiniApp

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import WebApp from "@twa-dev/sdk";
 import { createRoot } from 'react-dom/client';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import App from './App';
 import "./index.css";
 
+WebApp.ready();
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TonConnectUIProvider manifestUrl="http://localhost:5173/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://localhost:5173/tonconnect-manifest.json">
       <App />
     </TonConnectUIProvider> 
   </React.StrictMode>
