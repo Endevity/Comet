2a54e14370f9f5e9791cd92e8485bff8fa24507b
Adam Varady <146221892+NotRaijin@users.noreply.github.com>
2024-06-28 09:29:05 +0200

    MiniApp

--- frontend/src/task.jsx
+++ frontend/src/task.jsx
@@ -3,7 +3,6 @@ const Task = ({ description, reward, buttonClass, onButtonClick }) => {
         <div className="task">
             <img src="/assets/images/Task.svg" alt="Task" className="task-image" />
             <p className="task-p">{description}</p>
-            <img src="/assets/images/Token-Small.png" alt="Token" className="task-token" />
             <p className="task-reward">{reward}</p>
             <div className="task-button">
                 <div className={buttonClass} onClick={onButtonClick}>Go</div>
